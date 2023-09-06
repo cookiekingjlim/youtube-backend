92bd3226f5a46dc5f18df9696e7c88276d94aa08
ekato99 <133620407+cookiekingjlim@users.noreply.github.com>
2023-09-06 17:47:48 +0900

    feat: SpringBoot_Youtube_Model

---
+++ src/main/java/com/kh/youtube/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.kh.youtube.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("test", "집에 가쟈ㅑㅑㅑ");
+        return "index";
+    }
+}
